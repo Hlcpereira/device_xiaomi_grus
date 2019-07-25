c7ec57193d6a6fea9dc29d0f538bfc453c54bbdc
Henrique Pereira <hlcpereira@outlook.com.br>
2019-07-25 10:21:27 -0300

    grus: Import recovery fstab
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,11 +7,16 @@
 # Inherit from Xiaomi sdm710-common
 include device/xiaomi/sdm710-common/BoardConfigCommon.mk
 
+DEVICE_PATH := device/xiaomi/grus
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/grus/BoardConfigVendor.mk
