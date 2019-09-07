f4c9c369aad8628409dc0f644273304d17752845
argraur <argraur@gmail.com>
2019-09-07 07:32:18 +0000

    grus: copy libnfc-nci config to product
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -64,6 +64,6 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
